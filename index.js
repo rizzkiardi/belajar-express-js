e5638b569e26e5c9aebbaa5c483a4cbbaa87f8f9
rizzkiardi <rizkiseptiardi95@gmail.com>
2026-04-30 19:58:40 +0700

    add method GET and id params

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// GET, POST, PUT, PATCH, DELETE
 app.get("/", (req, res) => {
   res.send("Belajar express");
+});
+
+// GET
+app.get("/about", (req, res) => {
+  res.send("Ini halaman about");
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Users dengan id ${id}`);
 });
 
 app.listen(port, () => {
